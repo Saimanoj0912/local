c86d4f2f5739ec7c08dc7df9b4a332de508dcce2
Saimanoj0912 <20bq1a0479@vvit.net>
2023-02-16 15:39:44 +0530

    Sum of even numb

--- src/main/SumOfEvenNumb.java
+++ src/main/SumOfEvenNumb.java
@@ -1,20 +1,22 @@
-public class SumOfEvenNumbers {
+package main;
+
+public class SumOfEvenNumb {
     public static void main(String[] args) {
         int[] array = new int[]{1, 3, 2, 8, 15, 199};
 
         System.out.println(sum(array));
     }
 
-    public static int sum(int[] array){
+    public static int sum(int[] array) {
 
         //put your code here
-        int i=0, sum=0;
-        if (array==null) {
+        int i = 0, sum = 0;
+        if (array == null) {
             return 0;
         }
-        for (i=0;i<array.length;i++) {
-            if (array[i]%2==0) {
-                sum+=array[i];
+        for (i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                sum += array[i];
             }
         }
         return sum;
